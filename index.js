764aea4d44ee3cf95bc6ecbf840d6c8afd7d50ca
Adeilson Leal <adeilsonleal.al@gmail.com>
2025-02-27 13:22:25 -0300

    update

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
-import express from 'express'; // Importa o módulo express.
+import express, { query } from 'express'; // Importa o módulo express.
 import pool from './servico/conexao.js'; // Importa a conexão com o banco de dados.
-import { retornaCampeonatos } from './servico/retornaCampeonatos.js'; // Importa a função para retornar campeonatos.
+import { retornaCampeonatos, retornaCampeonatosID, retornaCampeonatosAno } from './servico/retornaCampeonatos.js'; // Importa a função para retornar campeonatos.
 
 const app = express(); // Cria uma instância do módulo express.
 
 app.get('/campeonatos', async (req, res) => { // Define uma rota GET para '/campeonatos'.
-    const campeonatos = await retornaCampeonatos(); // Chama a função retornaCampeonatos para obter os campeonatos.
-    res.json(campeonatos); // Envia os campeonatos como resposta em formato JSON.
+    let campeonatos;
+
+    const ano = req.query.ano;
+
+    if(typeof ano === 'undefined') {
+        campeonatos = await retornaCampeonatos();
+    } else {
+        campeonatos = await retornaCampeonatosAno(parseInt(ano));
+    }
+
+    if(campeonatos.length > 0) {
+        res.json(campeonatos);
+    } else {
+        res.status(404).json({mensagem: "Nenhum campeonato encontrado"})
+    }
 });
 
 app.listen(9000, async () => { // Configura o servidor para ouvir na porta 9000.
@@ -17,3 +30,15 @@ app.listen(9000, async () => { // Configura o servidor para ouvir na porta 9000.
     console.log(conexao.threadId); // Exibe o ID da thread da conexão no console.
     conexao.release(); // Libera a conexão de volta para o pool de conexões.
 });
+
+app.get('/campeonatos/:id', async (req, res) => {
+    const id = parseInt(req.params.id);
+
+    const campeonato = await retornaCampeonatosID(id);
+
+    if(campeonato.length > 0) {
+        res.json(campeonato);
+    } else {
+        res.status(404).json({mensagem: "Nenhum campeonato encontrado"});
+    }
+})
--- servico/retornaCampeonatos.js
+++ servico/retornaCampeonatos.js
@@ -11,3 +11,27 @@ export async function retornaCampeonatos() { // Exporta uma função assíncrona
 
     return campeonatos; // Retorna os resultados da consulta.
 }
+
+export async function retornaCampeonatosID(id) { // Exporta uma função assíncrona chamada retornaCampeonatosID.
+    const conexao = await pool.getConnection(); // Obtém uma conexão do pool de conexões.
+
+    const campeonatos_tb = await conexao.query('SELECT id, campeao, vice, ano FROM campeonatos WHERE id ='+id); // Executa uma consulta SQL que retorna as colunas id, campeao, vice e ano da tabela campeonatos de acordo com o ID especificado.
+
+    const campeonatos = campeonatos_tb[0]; // Armazena os resultados da consulta na variável campeonatos extraindo apenas os dados das linhas da tabela.
+
+    conexao.release(); // Libera a conexão de volta para o pool de conexões.
+
+    return campeonatos; // Retorna os resultados da consulta.
+}
+
+export async function retornaCampeonatosAno(ano) { // Exporta uma função assíncrona chamada retornaCampeonatosID.
+    const conexao = await pool.getConnection(); // Obtém uma conexão do pool de conexões.
+
+    const campeonatos_tb = await conexao.query('SELECT id, campeao, vice, ano FROM campeonatos WHERE ano ='+ano); // Executa uma consulta SQL que retorna as colunas id, campeao, vice e ano da tabela campeonatos de acordo com o ano especificado.
+
+    const campeonatos = campeonatos_tb[0]; // Armazena os resultados da consulta na variável campeonatos extraindo apenas os dados das linhas da tabela.
+
+    conexao.release(); // Libera a conexão de volta para o pool de conexões.
+
+    return campeonatos; // Retorna os resultados da consulta.
+}
